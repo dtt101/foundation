9e813ddb120bf1980316b09e749eb42f84f2deb9
David Thompson <vs@VS.local>
2016-09-01 21:20:15 +0100

    Switch DB to postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 User.create([
-  { username: 'foo', email: 'foo@example.com' },
-  { username: 'bar', email: 'bar@example.com' }
+  { username: 'foo', email: 'foo@example.com', password: 'password', password_confirmation: 'password' },
+  { username: 'bar', email: 'bar@example.com', password: 'password', password_confirmation: 'password' }
 ])
